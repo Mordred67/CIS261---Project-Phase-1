9ec5338c9093865ac6b3c99f8456582473b147eb
strayer_practice@outlook.com <strayer_practice@outlook.com>
2023-02-11 14:02:46 -0800

    Completed

--- CIS261___Project_Phase_1.py
+++ CIS261___Project_Phase_1.py
@@ -1,1 +1,75 @@
+# Joshua Holley CIS 261 Phase 1 Project
+#
+#
+def GetEmpName():
+    empname = input("Enter employee name: ")
+    return empname
+#for the next three functions, you need to convert the input to a float, e.g., varname = float(input('descripion of input:  '))
+#write the GetHoursWorked function
+GetHoursWorked = float(input("Enter Hours worked."))
+
+#write the GetHourlyRate function
+GetHourlyRate = float(input("Enter Hourly rate."))
+
+# write the GetTaxRate function
+GetTaxRate = float(input("Enter your tax rate."))
+
+
+
+def CalcTaxAndNetPay(hours, hourlyrate, taxrate):
+    grosspay = hours * hourlyrate
+    incometax = grosspay * taxrate
+    netpay = grosspay - incometax
+    return grosspay, incometax, netpay
+
+def printinfo(empname, hours, hourlyrate,grosspay, taxrate, incometax, netpay):
+    print("Name:  ", empname) 
+    print("Hours Worked: ", f"{hours:,.2f}")
+    # write the lines of code to display hourlyrate, grosspay, taxrate, incometax and netpay with correct formatting
+    # taxrate needs to be formatted as percentage
+    print("Hourly Rate: ", f"{hourlyrate:,.2f}")
+    print("Gross Pay: ", f"{grosspay:,.2f}")
+    print("Income Tax: ", f"{taxrate:,.2f}")
+    print("Net Pay: ", f"{incometax:,.2f}")
+    print("Tax Rate: ", f"{netpay:,.0%}")
+
+    print()
+def PrintTotals(TotEmployees, TotHours, TotGrossPay, TotTax, TotNetPay):    
+    print()
+    print(f"Total Number Of Employees: {TotEmployees}")
+    print(f"Total Hours Worked: {TotHours:,.2f}")
+    # write the code to print  TotGrossPay, TotTax, and TotNetpay with 2 decimal places
+    print(f"Total Hours Worked: {TotGrossPay:,.2f}")
+    print(f"Total Hours Worked: {TotTax:,.2f}")
+    print(f"Total Hours Worked: {TotNetPay:,.2f}")
+
+
+if __name__ == "__main__":
+    TotEmployees = 0
+    TotHours = 0.00
+    TotGrossPay = 0.00
+    TotTax = 0.00
+    TotNetPay = 0.00
+    while True:
+        empname = GetEmpName()
+        if (empname.upper() == "END"):
+            break
+        # write the code to assign to hours the return value from GetHoursWorked
+        hours = GetHoursWorked
+        # write the code to assign to hourlyrate the return value from GetHourlyRate
+        hourlyrate = GetHourlyRate
+        # write the code to assign to taxrate the return value from GetTaxRate
+        taxrate = GetTaxRate
+        grosspay, incometax, netpay = CalcTaxAndNetPay(hours, hourlyrate, taxrate)
+        printinfo(empname, hours, hourlyrate, grosspay, taxrate, incometax, netpay)
+        TotEmployees += 1
+        TotHours += hours
+        # write the code to increment the other total variables with the appropriate values
+
+
+
+    PrintTotals (TotEmployees, TotHours, TotGrossPay, TotTax, TotNetPay)
+
+
+
 
